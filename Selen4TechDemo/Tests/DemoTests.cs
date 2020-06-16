662430ce18ee49a30ee42b785f0e09e08e472260
ste ste <steelst0@gmail.com>
2020-06-16 15:22:35 -0700

    Adding Relational locator demo test

--- Selen4TechDemo/Tests/DemoTests.cs
+++ Selen4TechDemo/Tests/DemoTests.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace Selen4TechDemo.Tests
 {
@@ -14,6 +17,7 @@ namespace Selen4TechDemo.Tests
         //public void BasicSearch()
         //{
         //    //Arrange
+        //    DriverGc.Manage().Window.Maximize();
         //    DriverGc.Navigate().GoToUrl("http://www.google.com");
         //    DriverGc.FindElement(By.CssSelector("input[title*=\"Search\"]")).SendKeys("Parametric");
         //    //Act
@@ -31,23 +35,49 @@ namespace Selen4TechDemo.Tests
         /// Selenium 3 Code:
         /// driver.FindElement(By.CssSelector("body")).SendKeys(Keys.Control + "t");
         /// driver.SwitchTo().Window(driver.WindowHandles.Last());
-        /// driver.Navigate().GoToUrl("http://www.google.com")
         /// </summary>
         [Test]
-        public void DemoNewTab()
+        [Repeat(10)]
+        public void NewTabDemo()
         {
             //Arrange
+            Console.WriteLine("Navigating to page1, creating new tab, navigating to page2 in new tab");
             var originalTab = DriverGc.CurrentWindowHandle;
             DriverGc.Manage().Window.Maximize();
             DriverGc.Navigate().GoToUrl("http://www.google.com");
+            Thread.Sleep(2000);
             DriverGc.SwitchTo().NewWindow(WindowType.Tab);
             DriverGc.Navigate().GoToUrl("http://www.bing.com");
+            Thread.Sleep(2000);
             var newTab = DriverGc.CurrentWindowHandle;
             //Act
+            Console.WriteLine("Switch to first tab then back to new tab");
             DriverGc.SwitchTo().Window(originalTab);
+            Thread.Sleep(2000);
             DriverGc.SwitchTo().Window(newTab);
+            Thread.Sleep(2000);
             //Assert
+            Console.WriteLine("Validating that the current tab has expected title");
             Assert.IsTrue(DriverGc.Title.Contains("Bing"));
+        }
+
+        [Test]
+        public void RelativeLocatorsDemo()
+        {
+            //Arrange
+            Console.WriteLine("Navigating to page we want to locate an element on");
+            DriverGc.Navigate().GoToUrl("http://www.google.com");
+            Thread.Sleep(3000);
+            DriverGc.FindElement(By.CssSelector("input[title*=\"Search\"]")).SendKeys("Parametric");
+            var searchButton =
+                DriverGc.FindElement(By.Name("btnK"));
+            var luckyButton = DriverGc.FindElement(RelativeBy.WithTagName("input").RightOf(searchButton));
+            luckyButton.Click();
+            //Act
+            Console.WriteLine("Navigating to page we want to locate an element on");
+            Thread.Sleep(3000);
+            //Assert
+            //Assert.IsTrue(DriverGc.Url.Contains("wikipedia"));
         }
     }
 }
